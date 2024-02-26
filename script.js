6f9edc274170328d5d34395afd635db7da33c09e
JoseBlue17 <150461247+JoseBlue17@users.noreply.github.com>
2024-02-26 11:33:02 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+// Algoritmo de cálculo inicial
+
+
+
